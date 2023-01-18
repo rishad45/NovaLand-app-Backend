0d643101d7ee5bf35ad2f593dd54e46ffbf4b002
rishad paduvil <45rishadricu@gmail.com>
2023-01-18 12:23:58 +0530

    secnd chan

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
   'https://www.futurestore.com',
   'https://novaland.futurestore.website',
-  'https://startling-paletas-19f121.netlify.app/',
-  'http://startling-paletas-19f121.netlify.app/',
+  'https://kaleidoscopic-empanada-30caf7.netlify.app',
+  'https://kaleidoscopic-empanada-30caf7.netlify.app',
   'http://novaland.futurestore.website',
   'http://127.0.0.1',
   'http://localhost:3000',
