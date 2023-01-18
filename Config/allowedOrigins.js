fe7a9498de15fe373abef135efa3f8f98eab0220
rishad paduvil <45rishadricu@gmail.com>
2023-01-18 14:17:20 +0530

    secnd chan

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -7,8 +7,7 @@ const allowedOrigins = [
   'http://127.0.0.1',
   'http://localhost:3000',
   'http://localhost:5000',
-  'http://localhost:3001',
-  'http://localhost:3002',
+  'https://novaland.futurestore.website:8000',
 ];
 
 module.exports = allowedOrigins;
