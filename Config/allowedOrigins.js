6814c8c91169aed09b5002443b28529dbb544fac
rishad paduvil <45rishadricu@gmail.com>
2023-01-19 10:13:08 +0530

    Cors issue fixed!��

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
   'https://www.futurestore.com',
   'https://novaland.futurestore.website',
-  'https://singular-kashata-c47411.netlify.app',
-  'http://singular-kashata-c47411.netlify.app',
+  'https://vocal-faloodeh-5d3f47.netlify.app',
+  'http://vocal-faloodeh-5d3f47.netlify.app',
   'http://novaland.futurestore.website',
   'http://127.0.0.1',
   'http://localhost:3000',
